1f356fb6740d04a66456037b559c565d0fab9953
Shrishailya345 <karandeshrishailya@gmail.com>
2024-04-17 16:38:23 +0530

    updated code

---
+++ app/src/main/java/com/example/myfirstapplication/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.example.myfirstapplication;
+
+import static java.lang.Thread.*;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread thread=new Thread()
+        {
+            public void run() {
+            try {
+                sleep(2000);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+            finally {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+          }
+        };thread.start();
+
+    }
+}
